2cb5f776945d61891ae4064bed592e6d7ada3437
Zedong Yu <47105158+nacayu@users.noreply.github.com>
2023-12-04 21:09:08 +0800

    Update test_net.py

--- tools/test_net.py
+++ tools/test_net.py
@@ -1,130 +1,106 @@
-import json
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+# Set up custom environment before nearly anything else is imported
+# NOTE: this should be the first import (no not reorder)
+import argparse
 import os
-import sys
-from collections import defaultdict
-from concurrent import futures
 
-import cv2
-import matplotlib
-import numpy as np
+import torch
 
-# Use a non-interactive backend
-matplotlib.use('Agg')
-
-import matplotlib.pyplot as plt
-
-
-# Print iterations progress (thanks StackOverflow)
-def print_progress(iteration, total, prefix='', suffix='', decimals=1, bar_length=100):
-    """
-    Call in a loop to create terminal progress bar
-    @params:
-        iteration   - Required  : current iteration (Int)
-        total       - Required  : total iterations (Int)
-        prefix      - Optional  : prefix string (Str)
-        suffix      - Optional  : suffix string (Str)
-        decimals    - Optional  : positive number of decimals in percent complete (Int)
-        bar_length   - Optional  : character length of bar (Int)
-    """
-    formatStr = "{0:." + str(decimals) + "f}"
-    percents = formatStr.format(100 * (iteration / float(total)))
-    filledLength = int(round(bar_length * iteration / float(total)))
-    bar = '' * filledLength + '-' * (bar_length - filledLength)
-    sys.stdout.write('\r%s |%s| %s%s %s' % (prefix, bar, percents, '%', suffix)),
-    if iteration == total:
-        sys.stdout.write('\x1b[2K\r')
-    sys.stdout.flush()
+from fcos_core.config import cfg
+from fcos_core.data import make_data_loader
+from fcos_core.engine.inference import inference
+from fcos_core.modeling.detector import build_detection_model
+from fcos_core.utils.checkpoint import DetectronCheckpointer
+from fcos_core.utils.collect_env import collect_env_info
+from fcos_core.utils.comm import synchronize, get_rank
+from fcos_core.utils.env import setup_environment  # noqa F401 isort:skip
+from fcos_core.utils.logger import setup_logger
+from fcos_core.utils.miscellaneous import mkdir
 
 
-def vis_one_image(im, im_name, output_dir, boxes, thresh=0.508, dpi=200, box_alpha=0.8, show_class=True, ext='png',
-                  out_when_no_box=False):
-    """Visual debugging of detections."""
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
+def main():
+    parser = argparse.ArgumentParser(description="PyTorch Object Detection Inference")
+    parser.add_argument(
+        "--config-file",
+        default="configs/fcos_nuscenes/fcos_imprv_R_50_FPN_1x_ATTMIX_135_Circle_07.yaml",
+        metavar="FILE",
+        help="path to config file",
+    )
+    parser.add_argument(
+        "--checkpoint-file",
+        default="ckpts/model_0040000.pth",
+        metavar="FILE",
+        help="path to config file",
+    )
+    parser.add_argument("--local_rank", type=int, default=0)
+    parser.add_argument(
+        "opts",
+        help="Modify config options using the command-line",
+        default=None,
+        nargs=argparse.REMAINDER,
+    )
 
-    # if (boxes is None or boxes.shape[0] == 0 or max(boxes[:, 4]) < thresh) and not out_when_no_box:
-    #     return
+    args = parser.parse_args()
 
-    fig = plt.figure(frameon=False)
-    fig.set_size_inches(im.shape[1] / dpi, im.shape[0] / dpi)
-    ax = plt.Axes(fig, [0., 0., 1., 1.])
-    ax.axis('off')
-    fig.add_axes(ax)
-    ax.imshow(im)
+    num_gpus = int(os.environ["WORLD_SIZE"]) if "WORLD_SIZE" in os.environ else 1
+    distributed = num_gpus > 1
 
-    if boxes is None:
-        sorted_inds = []  # avoid crash when 'boxes' is None
-    else:
-        # Display in largest to smallest order to reduce occlusion
-        areas = boxes[:, 2] * boxes[:, 3]
-        sorted_inds = np.argsort(-areas)
+    if distributed:
+        torch.cuda.set_device(args.local_rank)
+        torch.distributed.init_process_group(
+            backend="nccl", init_method="env://"
+        )
+        synchronize()
 
-    for i in sorted_inds:
-        bbox = boxes[i, :4]
-        score = boxes[i, -1]
-        if score < thresh:
-            continue
-        # show box (off by default)
-        ax.add_patch(
-            plt.Rectangle((bbox[0], bbox[1]),
-                          bbox[2],
-                          bbox[3],
-                          fill=False, edgecolor='g',
-                          linewidth=0.5, alpha=box_alpha))
+    cfg.merge_from_file(args.config_file)
+    cfg.merge_from_list(args.opts)
+    cfg.freeze()
 
-        if show_class:
-            ax.text(
-                bbox[0], bbox[1] - 2, 'obstacle' + ' {:0.2f}'.format(score).lstrip('0'),
-                fontsize=3,
-                family='serif',
-                bbox=dict(
-                    facecolor='g', alpha=0.4, pad=0, edgecolor='none'),
-                color='white')
+    save_dir = cfg.OUTPUT_DIR
+    logger = setup_logger("fcos_core", save_dir, get_rank(), filename="test_all.txt")
+    logger.info("Using {} GPUs".format(num_gpus))
+    logger.info(cfg)
 
-    output_name = os.path.basename(im_name) + '.' + ext
-    fig.savefig(os.path.join(output_dir, '{}'.format(output_name)), dpi=dpi)
-    plt.close('all')
+    logger.info("Collecting env info (might take some time)")
+    logger.info("\n" + collect_env_info())
 
+    model = build_detection_model(cfg)
+    model.to(cfg.MODEL.DEVICE)
 
-def run(dic):
-    # your nuscenes data path
-    img = cv2.imread(os.path.join('/home/naca/Data/nuScenes', dic["file_name"]), cv2.IMREAD_COLOR)[:, :, ::-1]
-    basename = os.path.splitext(os.path.basename(dic["file_name"]))[0]
-    image_id = dic['id']
-    cls_box_i = []
-    for res in pred_by_image[image_id]:
-        item = list()
-        score = res['score']
-        box = res['bbox']
-        item.extend(box)
-        item.append(score)
-        cls_box_i.append(item)
-    cls_box_i = np.asarray(cls_box_i)
-    # your saf-fcos project path
-    vis_one_image(img, basename, '/home/naca/Projects/00_class_project/SAF-FCOS/' + config_name, cls_box_i)
+    checkpointer = DetectronCheckpointer(cfg, model)
+    model_name = args.checkpoint_file.split('/')[-1]
+    # uncomment following if you want to load pretrained model
+    _ = checkpointer.load(args.checkpoint_file)
+    # _ = checkpointer.load()
+    
+
+    iou_types = ("bbox",)
+    if cfg.MODEL.MASK_ON:
+        iou_types = iou_types + ("segm",)
+    if cfg.MODEL.KEYPOINT_ON:
+        iou_types = iou_types + ("keypoints",)
+    output_folders = [None] * len(cfg.DATASETS.TEST)
+    dataset_names = cfg.DATASETS.TEST
+    if cfg.OUTPUT_DIR:
+        for idx, dataset_name in enumerate(dataset_names):
+            output_folder = os.path.join(cfg.OUTPUT_DIR, "inference-" + model_name, dataset_name)
+            mkdir(output_folder)
+            output_folders[idx] = output_folder
+    data_loaders_val = make_data_loader(cfg, is_train=False, is_distributed=distributed)
+    for output_folder, dataset_name, data_loader_val in zip(output_folders, dataset_names, data_loaders_val):
+        inference(
+            model,
+            data_loader_val,
+            dataset_name=dataset_name,
+            iou_types=iou_types,
+            box_only=False if cfg.MODEL.FCOS_ON or cfg.MODEL.RETINANET_ON else cfg.MODEL.RPN_ONLY,
+            device=cfg.MODEL.DEVICE,
+            expected_results=cfg.TEST.EXPECTED_RESULTS,
+            expected_results_sigma_tol=cfg.TEST.EXPECTED_RESULTS_SIGMA_TOL,
+            output_folder=output_folder,
+        )
+        synchronize()
 
 
 if __name__ == "__main__":
-
-    config_name = 'fcos_imprv_R_50_FPN_1x_IMG'
-    # after run test_net.py, we could get bbox.json
-    json_file_path = '/home/naca/Projects/00_class_project/SAF-FCOS/inference-model_0040000.pth/nuscenes_test_cocostyle/bbox.json'
-    with open(json_file_path, "r") as f:
-        predictions = json.load(f)
-    pred_by_image = defaultdict(list)
-    for p in predictions:
-        pred_by_image[p["image_id"]].append(p)
-    # change your path to gt_fcos_coco_val.json
-    with open('/home/naca/Data/nuScenes/v1.0-trainval/gt_fcos_coco_val.json', 'r') as f:
-        dicts = json.load(f)
-        dicts = dicts['images']
-
-    print("Generating 2D detection results")
-    num_threads = 40
-    num_tokens = len(dicts)
-    with futures.ProcessPoolExecutor(max_workers=num_threads) as executor:
-        fs = [executor.submit(run, dic) for dic in
-              dicts]
-        for i, f in enumerate(futures.as_completed(fs)):
-            # Write progress to error so that it can be seen
-            print_progress(i, num_tokens, prefix='VisDetection', suffix='Done ', bar_length=40)
+    main()
